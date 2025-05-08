fd85cde51d45f267c00645812f2344358179599c
Thilankavishka <thilankavishka25@gmail.com>
2025-05-09 05:15:30 +0530

    add analytics

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Projects from './components/Projects';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 import Achievements from './components/Achievements';
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
      
       <Navbar />
       <Hero />
+      <Analytics />
       <About />
       <Achievements/>
       <Skills />
